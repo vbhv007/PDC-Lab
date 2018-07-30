90344f15aca1d28cc761643a94f20f6d7a963f54
vbhv007 <bhardwajvbhv@gmail.com>
2018-07-30 12:57:30 +0530

    updated algo for sections

--- LAB-3/shed_matrix_section.c
+++ LAB-3/shed_matrix_section.c
@@ -2,24 +2,29 @@
 #include <omp.h>
 
 int main(void){
-	int i, j, k, sum = 0;
-	int mat1[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-	int mat2[3][3] = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
-	int add[3][3];
-	int sub[3][3];
-	int mul[3][3];
-	int trans1[3][3];
-	int trans2[3][3];
-	int add_thread[3][3];
-	int sub_thread[3][3];
-	int mul_thread[3][3];
-	int t_thread[3][3];
+	int i, j, k, a = 5, b = 5, c = 5, sum = 0;
+	int mat1[a][b], mat2[a][b];
+	for (i = 0; i < a; i++){
+                for (j = 0; j < b; j++){
+                        mat1[i][j] = i + j;
+			mat2[i][j] = i - j;
+                }
+        }
+	int add[a][b];
+	int sub[a][b];
+	int mul[a][b];
+	int trans1[a][b];
+	int trans2[a][b];
+	int add_thread[a][b];
+	int sub_thread[a][b];
+	int mul_thread[a][b];
+	int t_thread[a][b];
 	#pragma omp parallel
 	{
 		#pragma omp sections
 		{
-			for (i = 0; i < 3; i++){
-				for (j = 0; j < 3; j++){
+			for (i = 0; i < a; i++){
+				for (j = 0; j < b; j++){
 					add[i][j] = mat1[i][j] + mat2[i][j];
 					add_thread[i][j] = omp_get_thread_num();
 				}
@@ -27,8 +32,8 @@ int main(void){
 		}
 		#pragma omp sections
                 {
-                        for (i = 0; i < 3; i++){
-                                for (j = 0; j < 3; j++){
+                        for (i = 0; i < a; i++){
+                                for (j = 0; j < b; j++){
                                         sub[i][j] = mat1[i][j] - mat2[i][j];
 					sub_thread[i][j] = omp_get_thread_num();
                                 }
@@ -36,9 +41,9 @@ int main(void){
                 }
 		#pragma omp sections
                 {
-                        for (i = 0; i < 3; i++){
-                                for (j = 0; j < 3; j++){
-					for (k = 0; k < 3; k++){
+                        for (i = 0; i < a; i++){
+                                for (j = 0; j < b; j++){
+					for (k = 0; k < c; k++){
 						sum += mat1[i][k] * mat2[k][j];
 					}
 					mul[i][j] = sum;
@@ -49,8 +54,8 @@ int main(void){
                 }
 		#pragma omp sections
                 {
-                        for (i = 0; i < 3; i++){
-                                for (j = 0; j < 3; j++){
+                        for (i = 0; i < a; i++){
+                                for (j = 0; j < b; j++){
                                         trans1[i][j] = mat1[j][i];
 					trans2[i][j] = mat2[j][i];
 					t_thread[i][j] = omp_get_thread_num();
@@ -60,50 +65,50 @@ int main(void){
 
 	}
 	printf("Original matrix 1:\n");
-	for (i = 0; i < 3; i++){
-                for (j = 0; j < 3; j++){
+	for (i = 0; i < a; i++){
+                for (j = 0; j < b; j++){
                         printf("%d\t", mat1[i][j]);
                 }
                 printf("\n");
         }
 	printf("\nOriginal matrix 2:\n");
-	for (i = 0; i < 3; i++){
-                for (j = 0; j < 3; j++){
+	for (i = 0; i < a; i++){
+                for (j = 0; j < b; j++){
                         printf("%d\t", mat2[i][j]);
                 }
                 printf("\n");
         }
 	printf("\nAddition:\n");
-	for (i = 0; i < 3; i++){
-		for (j = 0; j < 3; j++){
+	for (i = 0; i < a; i++){
+		for (j = 0; j < b; j++){
 			printf("%d(%d)\t", add[i][j], add_thread[i][j]);
 		}
 		printf("\n");
 	}
 	printf("\nSubtraction:\n");
-	for (i = 0; i < 3; i++){
-                for (j = 0; j < 3; j++){
+	for (i = 0; i < a; i++){
+                for (j = 0; j < b; j++){
                         printf("%d(%d)\t", sub[i][j], sub_thread[i][j]);
                 }
                 printf("\n");
         }
 	printf("\nMultiplication:\n");
-        for (i = 0; i < 3; i++){
-                for (j = 0; j < 3; j++){
+        for (i = 0; i < a; i++){
+                for (j = 0; j < b; j++){
                         printf("%d(%d)\t", mul[i][j], mul_thread[i][j]);
                 }
                 printf("\n");
         }
 	printf("\nTranspose of matrix 1:\n");
-        for (i = 0; i < 3; i++){
-                for (j = 0; j < 3; j++){
+        for (i = 0; i < a; i++){
+                for (j = 0; j < b; j++){
                         printf("%d(%d)\t", trans1[i][j], t_thread[i][j]);
                 }
                 printf("\n");
         }
 	printf("\nTranspose of matrix 2\n");
-        for (i = 0; i < 3; i++){
-                for (j = 0; j < 3; j++){
+        for (i = 0; i < a; i++){
+                for (j = 0; j < b; j++){
                         printf("%d(%d)\t", trans2[i][j], t_thread[i][j]);
                 }
                 printf("\n");

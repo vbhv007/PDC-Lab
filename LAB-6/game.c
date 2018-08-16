a8613d42ed559a2f3aa959e5e1f481417e34dbb0
vbhv007 <bhardwajvbhv@gmail.com>
2018-08-16 11:39:09 -0400

    lab6

---
+++ LAB-6/game.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<omp.h>
+
+int main(void){
+	int n = 9, i, j, count = 0;
+	//printf("Enter value of n: ");
+	//scanf("%d", &n);
+	//int A[n][n];
+	//for(i = 0; i < n; i++){
+	//	for(j = 0; j < n; j++){
+			//printf("\nEnter value of element (%d, %d): ", i, j);
+		       	//scanf("%d", &A[i][j]);
+	//	}
+	//}
+	int A[9][9] = {{0, 1, 1, 1, 0, 2, 1, 0, 0}, {0, 2, 1, 1, 0, 2, 1, 2, 2},
+		{0, 1, 1, 0, 2, 2, 0, 1, 1}, {1, 1, 1, 0, 2, 1, 0, 0, 0},
+		{1, 1, 2, 0, 0, 2, 2, 2, 1}, {1, 0, 0, 0, 2, 1, 2, 1, 1},
+		{0, 1, 1, 1, 0, 1, 1, 1, 0}, {2, 1, 0, 0, 1, 1, 2, 1, 2},
+		{1, 2, 2, 0, 1, 2, 0, 2, 1}};
+	#pragma omp parallel
+	{
+	#pragma omp sections
+	{
+	for(i = 0; i < (n - 1); i++){
+		for(j = 0; j < (n - 2); j++){
+			if((A[i][j] == A[i][j+1]) && (A[i][j+1] == A[i][j+2])){
+				count++;
+				printf("%d, %d(l2r), T = %d\n", i, j, omp_get_thread_num()); 	
+			}
+		}
+	}
+	}
+	#pragma omp sections
+	{
+	for(i = 0; i < (n - 2); i++){
+                for(j = 0; j < (n - 1); j++){
+                        if((A[i][j] == A[i+1][j]) && (A[i+1][j] == A[i+2][j])){
+                                count++;
+                                printf("%d, %d(t2b), T = %d\n", i, j, omp_get_thread_num());
+                        }
+                }
+        }
+	}
+	}
+	printf("Total: %d\n", count);
+	return 0;
+}	
+

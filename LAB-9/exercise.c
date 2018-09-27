3ff9a1fdf70d47befb2982da6839281d92023ab1
vbhv007 <bhardwajvbhv@gmail.com>
2018-09-27 11:38:40 -0400

    LAB-9 updated

--- LAB-9/exercise.c
+++ LAB-9/exercise.c
@@ -36,6 +36,7 @@ int is_armstrong(int num){
 int main(int argc, char *argv[]){
 	MPI_Init(&argc, &argv);
 	int rank, size;
+	char prime, armstrong;
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
         MPI_Comm_size(MPI_COMM_WORLD, &size);
         int number;
@@ -48,12 +49,22 @@ int main(int argc, char *argv[]){
                 MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		printf("The number %d is recieved by process %d, Checking if the number is prime\n", number, rank);
 		if (is_prime(number)){
+			prime = 'Y';
 			printf("The number %d is prime\n", number);
 		}
 		else{
+			prime = 'N';
 			printf("The number %d is not prime\n", number);
 		}
-		MPI_Send(&number, 1, MPI_INT, 2, 0, MPI_COMM_WORLD);
+		MPI_Send(&prime, 1, MPI_INT, 2, 0, MPI_COMM_WORLD);
+		if (is_armstrong(number)){
+			armstrong = 'Y';
+                        printf("The number %d is armstrong\n", number);
+                }
+                else{
+			armstrong = 'N';
+                        printf("The number %d is not armstrong\n", number);
+                }
                 printf("The number %d is sent by process %d\n", number, rank);
         }
 	else if (rank == 2){
